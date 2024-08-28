15f8874a2e645c74f314d5eebbef8ce2e90f6bff
Keny 1 <keny.almazan@mrm.com>
2024-08-28 12:50:55 -0600

    6to commit - Variables y estructuras de control de flujo

--- script/script.js
+++ script/script.js
@@ -1,14 +1,74 @@
-const boton = document.getElementById("boton");
+// var edad = prompt("Dame tu edad");
+// var nombre = prompt("Dame tu nombre");
+// var apellido = prompt("Dame tu apellido");
+// var pasatiempo = prompt("Dame tu pasatiempo");
+// var soltero = prompt("¿Eres soltero (a)?");
 
-boton.addEventListener('click', () => {
-    
-    
-    var textoBoton = boton.innerText;
+// var datos = "Mi nombre es: " + nombre + " " + apellido + "; Mi edad es: " + edad + " años; Mi pasatiempo es: " + pasatiempo + "; ¿Estoy casado? " + soltero
 
-    if(textoBoton.includes("Contacto")) {
-        alert("Clic en botón contacto");
-    } else {
-        alert("Clic en otro botón");
+// alert(datos);
+
+
+/*Se reasignan valores a las variables*/
+
+// edad = 34;
+
+// nombre = "Keny";
+// apellido = "Almazán";
+// pasatiempo = "Aprender programación";
+// soltero = false
+
+// datos = "Mi nombre es: " + nombre + " " + apellido + "; Mi edad es: " + edad + " años; Mi pasatiempo es: " + pasatiempo + "; ¿Estoy casado? " + soltero
+
+// console.log(datos)
+
+//Ciclo for se usa cuando conoces el número de iteraciones que llevarás a cabo
+
+for(var i = 10; i > 0; i--) {
+    console.log("El valor de i con for es: " + i);
+}
+
+var i = 0;
+
+while(i <= 10) {
+    console.log("El valor de i con while es: " + i);
+    i++;
+}
+
+//If permite decidir qué bloque se sentencias se ejecuta en función de si se cumple o no una condición
+
+var numero = 8;
+var numeroTexto = "8";
+
+if(numero === numeroTexto) {
+    console.log("Los números son iguales en valor");
+}else {
+    console.log("Los números son iguales en valor pero no en tipo");
+}
+
+var soltero = false;
+
+if(soltero === true) {
+    console.log("Es soltero");
+}else {
+    console.log("No es soltero");
+}
+
+var edad = 18;
+var genero = "elle";
+
+if(edad >= 18) {
+    console.log("Puedes pasar el primer filtro");
+    if(genero == "femenino") {
+        console.log("Puedes pasar porque es un bar para muejeres");
+    }else {
+        console.log("Busca un bar para hombres");
     }
-})
+}else {
+    console.log("No puedes pasar el primer filtro");
+}
+
+
+
+
 
